cfd38fbe33b34a1c77dfef22676cc11c79fd2738
Vish Mullapudi <Vishruth.Mullapudi@UTSouthwestern.edu>
2025-05-26 16:07:05 -0500

    implements unimplemented myimageclass features and improves type annotations

--- 04_ExerciseCodeAndInstructions/imageio/MyImageClass.py
+++ 04_ExerciseCodeAndInstructions/imageio/MyImageClass.py
@@ -2,6 +2,7 @@ from re import L
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from collections.abc import Sequence
 
 #define an image class that has useful functions we can use!
 
@@ -13,7 +14,7 @@ class MyImgClass():
 
     """
     
-    def __init__(self, arrImg, intLabel=None):
+    def __init__(self, arrImg: np.ndarray, intLabel=None):
         """Initalize the MyImgClass object
 
         :param arrImg: a numpy array that contains the image
@@ -25,60 +26,57 @@ class MyImgClass():
         self.intLabel = intLabel
         self.shape = self.arrImg.shape
         
-    def __add__(self, other):
+    def __add__(self, other: "MyImgClass") -> "MyImgClass":
         """
         operator overloading for the '+' operation 
         element wise addtion of two MyImgClass together.
 
-        :param other: the other instance of arrImgClass
-        :type other: arrImgClass
+        :param other: the other instance of MyImgClass
+        :type other: MyImgClass
         :return: the addition of the two classes
         :rtype: MyImgClass
         """
-        toReturn = MyImgClass(np.add(self.arrImg, other.arrImg), intLabel=None)
-        return toReturn
+        #toReturn = MyImgClass(np.add(self.arrImg, other.arrImg), intLabel=None)
+        return MyImgClass(np.add(self.arrImg, other.arrImg), intLabel=None)
     
-    def __sub__(self, other):
+    def __sub__(self, other: "MyImgClass") -> "MyImgClass":
         """
         operator overloading for the '-' operation 
         element wise subtraction of two MyImgClass together.
 
-        :param other: the other instance of arrImgClass
-        :type other: arrImgClass
+        :param other: the other instance of MyImgClass
+        :type other: MyImgClass
         :return: the subtraction of the two classes
         :rtype: MyImgClass
         """
-        ### TO IMPLEMENT ###
-        raise NotImplementedError('You need to implement this method!')
+        return MyImgClass(np.subtract(self.arrImg, other.arrImg), intLabel=None)
         
     
-    def fPixelwiseSqDif(self, other):
+    def fPixelwiseSqDif(self, other: "MyImgClass") -> "MyImgClass":
         """Find the square difference between two MyImgClass objects
 
-        :param other: the other instance of arrImgClass
-        :type other: arrImgClass
+        :param other: the other instance of MyImgClass
+        :type other: MyImgClass
         :return: square difference of each pixel
         :rtype: MyImgClass
         """
-        ### TO IMPLEMENT ###
-        # Use the overloaded '-' from above
-        raise NotImplementedError('You need to implement this method!')
+        
+        return MyImgClass((self.arrImg - other.arrImg) ** 2)
     
     
     
-    def fMSE(self, other):
+    def fMSE(self, other: "MyImgClass") -> np.floating:
         """Find the mean squared error between two images
 
-        :param other: the other instance of arrImgClass
-        :type other: arrImgClass
+        :param other: the other instance of MyImgClass
+        :type other: MyImgClass
         :return: MSE
         :rtype: float
         """
-        ### TO IMPLEMENT ###
-        raise NotImplementedError('You need to implement this method!')
+        return np.mean(self.fPixelwiseSqDif(other).arrImg)
 
     
-    def fPlot(self, ax, show_ticks=False, add_colorbar=False, imshow_kwargs={}):
+    def fPlot(self, ax: plt.Axes, show_ticks=False, add_colorbar=False, imshow_kwargs={}):
         """Plotting method for the class
 
         :param ax: the axis to plot on
@@ -102,32 +100,55 @@ class MyImgClass():
             plt.colorbar(img, cax=cax, orientation='vertical')        
         
     @staticmethod
-    def fComputeMeanAcrossImages(lMyImgClass):
-        """Calcualte the mean image from a list of images
+    def fComputeMeanAcrossImages(lMyImgClass: Sequence["MyImgClass"]) -> "MyImgClass":
+        """Calculate the mean image from a list of images
 
         :param lMyImgClass: list of MyImgClass
-        :type lMyImgClass: list (or 1D iterable)
+        :type lMyImgClass: Sequence["MyImgClass"]
         :return: The mean image
         :rtype: MyImgClass
         """
-        ### TO IMPLEMENT ###
-        raise NotImplementedError('You need to implement this method!')
+        if len(lMyImgClass) == 0:
+            raise ValueError('Empty list provided. Please provide a list with at least one image.')
+        if not all(isinstance(img, MyImgClass) for img in lMyImgClass):
+            raise TypeError('All elements in the list must be instances of MyImgClass')
+        
+        # Stack images along a new dimension and compute the mean
+        # Ensure all images have the same shape
+        shapes = [img.arrImg.shape for img in lMyImgClass]
+        if len(set(shapes)) > 1:
+            raise ValueError('All images must have the same shape to compute the mean.')
+        
+        stacked_images = np.stack([img.arrImg for img in lMyImgClass], axis=0)
+        return MyImgClass(np.mean(stacked_images, axis=0))
+  
     
     @staticmethod
-    def fComputeStdAcrossImages(lMyImgClass):
-        """Calcualte the std image from a list of images
+    def fComputeStdAcrossImages(lMyImgClass: Sequence["MyImgClass"]) -> "MyImgClass":
+        """Calculate the std image from a list of images
 
         :param lMyImgClass: list of MyImgClass
         :type lMyImgClass: list (or 1D iterable)
         :return: The std image
         :rtype: MyImgClass
         """
-        ### TO IMPLEMENT ###
-        raise NotImplementedError('You need to implement this method!')
+        if len(lMyImgClass) == 0:
+            raise ValueError('Empty list provided. Please provide a list with at least one image.')
+        if not all(isinstance(img, MyImgClass) for img in lMyImgClass):
+            raise TypeError('All elements in the list must be instances of MyImgClass')
+        
+        # Stack images along a new dimension and compute the mean
+        # Ensure all images have the same shape
+        shapes = [img.arrImg.shape for img in lMyImgClass]
+        if len(set(shapes)) > 1:
+            raise ValueError('All images must have the same shape to compute the mean.')
+        
+        stacked_images = np.stack([img.arrImg for img in lMyImgClass], axis=0)
+        return MyImgClass(np.std(stacked_images, axis=0))
         
 
     @staticmethod
-    def fMeanMSE(lImg1, lImg2):
+    def fMeanMSE(lImg1: Sequence["MyImgClass"], lImg2: Sequence["MyImgClass"]) -> np.floating:
         """Calcualte the mean MSE across pairs of images
         e.g. mean(MSE(lImg1[0],lImg2[0]),MSE(lImg1[1],lImg2[1])...)
 
@@ -138,8 +159,7 @@ class MyImgClass():
         :return: mean MSE
         :rtype: float
         """
-        ### TO IMPLEMENT ###
-        raise NotImplementedError('You need to implement this method!')
+        return np.mean([img1.fMSE(img2) for img1, img2 in zip(lImg1, lImg2)])
 
 
     @staticmethod
